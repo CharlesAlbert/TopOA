f52a23b70903c35b2f4be5c0fdc0ee013de62cb1
Brian Elgaard Bennett <bb@saxobank.com>
2017-06-09 13:27:35 +0200

    Made the example depend on the .Net Core DI container

---
+++ TopOA.Example.Console/IoC/DefaultCompositionRoot.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Extensions.DependencyInjection;
+using TopOA.Core;
+using TopOA.Core.Commands;
+using TopOA.Core.Readers;
+
+namespace TopOA.Example.Console.IoC
+{
+    internal class DefaultCompositionRoot
+    {
+        public IServiceProvider Initialize()
+        {
+            var loginCommandhandler = new LoginCommandHandler();
+            var positionsReader = new PositionsReader();
+            IServiceProvider serviceProvider = new ServiceCollection()
+                .AddSingleton<IValidator<LoginCommand>>(loginCommandhandler)
+                .AddSingleton<ICommandHandler<LoginCommand>>(loginCommandhandler)
+                .AddSingleton<IValidator<PositionsQuery>>(positionsReader)
+                .AddSingleton<IQueryHandler<PositionsQuery, PositionsList>>(positionsReader)
+                .BuildServiceProvider();
+
+            return serviceProvider;
+        }
+    }
+}
--- TopOA.Example.Console/Program.cs
+++ TopOA.Example.Console/Program.cs
@@ -1,30 +1,36 @@
-﻿using TopOA.Core;
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using TopOA.Core;
 using TopOA.Core.Commands;
 using TopOA.Core.Readers;
+using TopOA.Example.Console.IoC;
 
 namespace TopOA.Example.Console
 {
-    class Program
+    public static class Program
     {
         /// <summary>The 24-hour token from the developer portal.</summary>
         private const string Token = "eyJhbGciOiJFUzI1NiIsIng1dCI6IkQ0QUU4MjQ2RDYyNTBFMTY5Njg4NDFCREY4Nzc2MTI4NUMwNUJCMUYifQ.eyJvYWEiOiI3Nzc3MCIsImlzcyI6Im9hIiwiYWlkIjoiMTA5IiwidWlkIjoianpkR0Zmc2tjV2lhUHVSZ0pXdVRZZz09IiwiY2lkIjoianpkR0Zmc2tjV2lhUHVSZ0pXdVRZZz09IiwiaXNhIjoiRmFsc2UiLCJ0aWQiOiIyMDAyIiwic2lkIjoiZDIwYTVlZmFhYjVlNDVjNmEzMTIyMzZiMzJhNDI5MDkiLCJkZ2kiOiI4NCIsImV4cCI6IjE0OTcwODM5NzAifQ.Q_gg9WMKMdGmw2D_X6TGVEz9Rni7X5TK_5jGNGJS6Gj9d-k_WXLZOZfzh1gtNtgSX-caeFjR_k6HlYx20e7ApQ";
 
         static void Main(string[] _)
         {
+            // Use the .Net Core DI container:
+            IServiceProvider serviceProvider = new DefaultCompositionRoot().Initialize();
+
+            // Log in:
             var loginCommand = new LoginCommand(Token);
-            IValidator<LoginCommand> loginValidator = new LoginCommandHandler();
+            var loginValidator = serviceProvider.GetService<IValidator<LoginCommand>>();
             if (loginValidator.Validate(loginCommand) != ErrorCode.NoError)
                 return;
-
-            var loginCommandHandler = (ICommandHandler<LoginCommand>)loginValidator;
+            var loginCommandHandler = serviceProvider.GetService<ICommandHandler<LoginCommand>>();
             loginCommandHandler.Execute(loginCommand);
 
+            // Get a list of positions:
             var posistionsQuery = new PositionsQuery();
-            IValidator<PositionsQuery> positionsValidator = new PositionsReader();
+            var positionsValidator = serviceProvider.GetService<IValidator<PositionsQuery>>();
             if (positionsValidator.Validate(posistionsQuery) != ErrorCode.NoError)
                 return;
-
-            var positionsReader = (IQueryHandler<PositionsQuery, PositionsList>)positionsValidator;
+            var positionsReader = serviceProvider.GetService<IQueryHandler<PositionsQuery, PositionsList>>();
             PositionsList positions = positionsReader.Query(posistionsQuery);
         }
     }
